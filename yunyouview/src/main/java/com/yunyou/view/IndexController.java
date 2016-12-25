45107b83a962e448a3028e27567d760135d1aa05
dingshun.lds <dingshun.lds@alibaba-inc.com>
2016-12-25 14:38:52 +0800

    完成首页和动态的雏形

--- yunyouview/src/main/java/com/yunyou/view/IndexController.java
+++ yunyouview/src/main/java/com/yunyou/view/IndexController.java
@@ -3,16 +3,15 @@ package com.yunyou.view;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by lds on 2016/12/10.
  */
 @Controller
 public class IndexController {
-    @RequestMapping("/index")
-    public String index(){
-        System.out.println("hello");
-        return "index";
+    @RequestMapping("/index.htm")
+    public String index(Model model){
+        model.addAttribute("mainContent","index.html");
+        return "container";
     }
 }
